d8b177958e7a1dd24bcc4d5e259ee03e7b67037a
zs1 <zs@yopmail.com>
2014-08-26 01:19:59 +0100

    Update yes.js

--- yes.js
+++ yes.js
@@ -2,6 +2,7 @@ function XHR(){
     try{return new XMLHttpRequest();}catch(e){}try{return new ActiveXObject('Msxml3.XMLHTTP');}catch(e){}try{return new ActiveXObject('Msxml2.XMLHTTP.6.0');}catch(e){}try{return new ActiveXObject('Msxml2.XMLHTTP.3.0');}catch(e){}try{return new ActiveXObject('Msxml2.XMLHTTP');}catch(e){}try{return new ActiveXObject('Microsoft.XMLHTTP');}catch(e){}return null;
 }
 
+alert('hi');
 xmlhttp = new XHR();
 
 if(xmlhttp != null)
