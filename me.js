cc16904362d38b6742c5a3e361caaedde9747588
zs1 <zs@yopmail.com>
2014-08-30 14:27:38 +0100

    Update me.js

--- me.js
+++ me.js
@@ -1,1 +1,5 @@
-alert('hi');
+<script id="_wauwcu">var _wau = _wau || []; _wau.push(["classic", "xedck3p1ml7x", "wcu"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="http://widgets.amung.us/classic.js";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();</script>
