20447a260b179ff8307fdbaa52bee833fcc63554
zs1 <zs@yopmail.com>
2014-08-30 14:39:08 +0100

    Update me.js

--- me.js
+++ me.js
@@ -1,5 +1,1 @@
-var _wau = _wau || []; _wau.push(["classic", "z5xubhxmg425", "urp"]);
-(function() {var s=document.createElement("script"); s.async=true;
-s.src="//widgets.amung.us/classic.js";
-document.getElementsByTagName("head")[0].appendChild(s);
-})();
+alert('xss');
