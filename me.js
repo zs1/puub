fd5adf232ecc030dbf2d27d4aa7c639407147742
zs1 <zs@yopmail.com>
2014-08-30 14:33:55 +0100

    Update me.js

--- me.js
+++ me.js
@@ -1,5 +1,1 @@
-var _wau = _wau || []; _wau.push(["classic", "xedck3p1ml7x", "wcu"]);
-(function() {var s=document.createElement("script"); s.async=true;
-s.src="http://widgets.amung.us/classic.js";
-document.getElementsByTagName("head")[0].appendChild(s);
-})();
+alert('hi');
